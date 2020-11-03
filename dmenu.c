9f262705ee215ef2f663a7ea9244aa6b4290b5bd
Mike Hoskins <mrh@devopsdreams.io>
2020-11-02 21:48:08 -0500

    update usage

--- dmenu.c
+++ dmenu.c
@@ -788,7 +788,7 @@ setup(void)
 static void
 usage(void)
 {
-	fputs("usage: dmenu [-bfiv] [-l lines] [-p prompt] [-fn font] [-m monitor]\n"
+	fputs("usage: dmenu [-bFfsv] [-l lines] [-p prompt] [-fn font] [-m monitor]\n"
 	      "             [-h height]\n"
 	      "             [-nb color] [-nf color] [-sb color] [-sf color] [-w windowid]\n", stderr);
 	exit(1);
