b937a8e2841b6c384b9ac71fd8809a9b03de3945
Mike Hoskins <mrh@devopsdreams.io>
2021-06-29 21:32:46 -0400

    fix missing constant

--- config.h
+++ config.h
@@ -16,7 +16,8 @@ static const char *colors[SchemeLast][2] = {
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
-static unsigned int lineheight = 0;         /* -h option; minimum height of a menu line     */
+static unsigned int lineheight = 0;         /* -h option; minimum height of a menu line */
+static unsigned int min_lineheight = 8;     /* reasonable default in case value too small/negative */
 
 /*
  * Characters not considered part of a word while deleting words
